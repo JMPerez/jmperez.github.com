958237fb48cf06d232b9d30b5531011725aa7ae5
JMPerez <jmperez1985@gmail.com>
2014-01-07 22:22:44 +0100

    Minified CSS through Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    cssmin: {
+      combine: {
+        files: {
+          'css/style.css': ['css/main.css', 'css/syntax.css']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['cssmin']);
+
+};
